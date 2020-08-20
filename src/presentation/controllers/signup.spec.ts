dc0603bb0f00e89f8c9628ace29e3cdfafa590c3
Victor Freitas <victornobrega1980@gmail.com>
2020-08-20 00:52:24 -0300

    refactor: add costomized error for missing param

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,4 +1,5 @@
 import { SignupController } from './signup';
+import { MissingParamError } from '../error/missing-param-error';
 
 describe('Signup Controller', () => {
   test('Should return 400 if no name is provider', () => {
@@ -12,7 +13,7 @@ describe('Signup Controller', () => {
     };
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new Error('Missing param: name'));
+    expect(httpResponse.body).toEqual(new MissingParamError('name'));
   });
 });
 
@@ -28,6 +29,6 @@ describe('Signup Controller', () => {
     };
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new Error('Missing param: email'));
+    expect(httpResponse.body).toEqual(new MissingParamError('email'));
   });
 });
