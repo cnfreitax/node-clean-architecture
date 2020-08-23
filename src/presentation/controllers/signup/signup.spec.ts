2cb12338a9a902891fd39831a0b91ef9ca98ae2e
Victor Freitas <victornobrega1980@gmail.com>
2020-08-23 00:57:17 -0300

    test: ensure SignUpController return 200 if valid data is provider

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -215,4 +215,24 @@ describe('Signup Controller', () => {
       password: 'any_password',
     });
   });
+
+  test('Should return 200 if data correct is provider ', () => {
+    const { sut } = makeSut();
+    const httpRequest = {
+      body: {
+        name: 'valid_name',
+        email: 'valid@mail.com',
+        password: 'valid_password',
+        passwordConfirmation: 'valid_password',
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(200);
+    expect(httpResponse.body).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid@mail.com',
+      password: 'valid_password',
+    });
+  });
 });
