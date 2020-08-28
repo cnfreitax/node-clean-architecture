584d2ae62ba16b76877b06724fd53f186686a56b
Victor Freitas <victornobrega1980@gmail.com>
2020-08-28 00:52:59 -0300

    refactor: done correct imports

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,12 +1,12 @@
 import { AddAccountRepository } from '../../../../data/protocols/add-account-repository';
 import { AddAccountModel } from '../../../../domain/usecases/addAccount';
 import { AccountModel } from '../../../../domain/models/account';
-import { MongoHelpepr } from '../helpers/mongo-helepr';
+import { MongoHelper } from '../helpers/mongo-helper';
 
 export class AccountMongoRepository implements AddAccountRepository {
   async add(accountData: AddAccountModel): Promise<AccountModel> {
-    const accountCollection = MongoHelpepr.getCollection('accounts');
+    const accountCollection = MongoHelper.getCollection('accounts');
     const result = await accountCollection.insert(accountData);
-    return MongoHelpepr.map(result.ops[0]);
+    return MongoHelper.map(result.ops[0]);
   }
 }
