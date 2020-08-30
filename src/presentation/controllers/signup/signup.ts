bcec4476216f3fc3cac7f394ec31e3223b3f53a0
Victor Freitas <victornobrega1980@gmail.com>
2020-08-30 01:38:25 -0300

    feat: ensure logControllerDecorator calls logErrorRepository if controller returns an serverError

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -49,7 +49,7 @@ export class SignupController implements Controller {
 
       return ok(account);
     } catch (error) {
-      return serverError();
+      return serverError(error);
     }
   }
 }
--- src/presentation/error/server-error.ts
+++ src/presentation/error/server-error.ts
@@ -1,6 +1,7 @@
 export class ServerError extends Error {
-  constructor() {
+  constructor(stack: string) {
     super('Hi! An unexpected error has ocurred. Try again later.');
     this.name = 'ServerError';
+    this.stack = stack;
   }
 }
