21c6d5bf4eb655fb5249c9b12a05e76b2401e388
Victor Freitas <victornobrega1980@gmail.com>
2020-08-30 16:47:32 -0300

    refactor: moved LogMongoRepository to helper method makeSut

--- src/infra/db/mongodb/log-resopository/log.spec.ts
+++ src/infra/db/mongodb/log-resopository/log.spec.ts
@@ -2,6 +2,10 @@ import { MongoHelper } from '../helpers/mongo-helper';
 import { Collection } from 'mongodb';
 import { LogMongoRepository } from './log';
 
+const makeSut = (): LogMongoRepository => {
+  return new LogMongoRepository();
+};
+
 describe('Log Mongo Repsoitory', () => {
   let errorsCollections: Collection;
 
@@ -18,7 +22,7 @@ describe('Log Mongo Repsoitory', () => {
     await errorsCollections.deleteMany({}); // delete datas
   });
   test('Should create an error log on success', async () => {
-    const sut = new LogMongoRepository();
+    const sut = makeSut();
     await sut.logError('any_error');
     const count = await errorsCollections.countDocuments();
     expect(count).toBe(1);
