67d14562142e05384a964a223f96ec3cd2033cf9
Victor Freitas <victornobrega1980@gmail.com>
2020-08-31 23:15:46 -0300

    refactor: moved makestu creator to a helper method

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -2,9 +2,20 @@ import { LoginController } from './login';
 import { badResquest } from '../../helpers/http-helpers';
 import { MissingParamError } from '../../error';
 
+interface SutType {
+  sut: LoginController;
+}
+
+const makeSut = (): SutType => {
+  const sut = new LoginController();
+  return {
+    sut,
+  };
+};
+
 describe('Login Controller', () => {
   test('Should return 400 if email is not provided', async () => {
-    const sut = new LoginController();
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
         password: 'any_password',
@@ -15,7 +26,7 @@ describe('Login Controller', () => {
   });
 
   test('Should return 400 if password is not provided', async () => {
-    const sut = new LoginController();
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
         email: 'any@mail.com',
