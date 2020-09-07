5712985f7cc49e6d303571cfedfc2a09a5c11b3f
Victor Freitas <victornobrega1980@gmail.com>
2020-09-07 04:52:25 -0300

    refactor: move sut to a helper method

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -1,17 +1,38 @@
 import { ValidationComposite } from './validation-composite';
 import { Validation } from './validation';
+import { MissingParamError } from '../../error';
+
+interface SutTypes {
+  sut: ValidationComposite;
+  validationStub: Validation;
+}
+
+const makeValidationStub = (): Validation => {
+  class ValidationStub implements Validation {
+    validate(data: any): Error {
+      return null;
+    }
+  }
+
+  return new ValidationStub();
+};
+
+const makeSut = (): SutTypes => {
+  const validationStub = makeValidationStub();
+  const sut = new ValidationComposite([validationStub]);
+  return {
+    sut,
+    validationStub,
+  };
+};
 
 describe('ValidatioComposite', () => {
   test('Should return a error if any validation fails', () => {
-    class ValidationStub implements Validation {
-      validate(data: any): Error {
-        return new Error();
-      }
-    }
-
-    const validationStub = new ValidationStub();
-    const sut = new ValidationComposite([validationStub]);
+    const { sut, validationStub } = makeSut();
+    jest
+      .spyOn(validationStub, 'validate')
+      .mockReturnValueOnce(new MissingParamError('field'));
     const error = sut.validate({ field: 'any_data' });
-    expect(error).toEqual(new Error());
+    expect(error).toEqual(new MissingParamError('field'));
   });
 });
