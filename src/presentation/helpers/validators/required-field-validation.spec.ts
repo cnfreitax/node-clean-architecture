1950615cf726faba0b7490032ac9ba1d380f548d
Victor Freitas <victornobrega1980@gmail.com>
2020-09-07 04:28:40 -0300

    refactor: move RequiredFieldValitadion to a method helper

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -1,15 +1,19 @@
 import { RequiredFieldValidiation } from './required-field-validation';
 import { MissingParamError } from '../../error';
 
+const makeSut = (): RequiredFieldValidiation => {
+  return new RequiredFieldValidiation('field');
+};
+
 describe('RequiredField Validation', () => {
   test('Should return a MissingParam error if validation fails', () => {
-    const sut = new RequiredFieldValidiation('field');
+    const sut = makeSut();
     const error = sut.validate({ name: 'name' });
     expect(error).toEqual(new MissingParamError('field'));
   });
 
   test('Should not return error if validation success', () => {
-    const sut = new RequiredFieldValidiation('field');
+    const sut = makeSut();
     const error = sut.validate({ field: 'name' });
     expect(error).toBeFalsy();
   });
