c2f1a2a9315acc2e2e9cc5e2fc323e748075c263
Victor Freitas <victornobrega1980@gmail.com>
2020-09-07 16:33:29 -0300

    refactor: move validation to protocols folder

--- src/presentation/helpers/validators/email-validation.ts
+++ src/presentation/helpers/validators/email-validation.ts
@@ -1,4 +1,4 @@
-import { Validation } from './validation';
+import { Validation } from '../../protocols/validation';
 import { EmailValidator } from '../../protocols/email-validotr';
 import { InvalidParamError } from '../../error';
 
