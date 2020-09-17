75276dc30dcf282373e378c2b69eaeca63d298aa
Victor Freitas <victornobrega1980@gmail.com>
2020-09-16 22:31:20 -0300

    feat: add login-factorie

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
   port: process.env.PORT || 5050,
+  jwtSecret: process.env.JWT_SECRET || '83b03d681a9201300565991d735c77a2',
 };
