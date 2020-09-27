4dcdef9ab986e349774609a46a1f971c560cf592
Victor Freitas <victornobrega1980@gmail.com>
2020-09-27 02:52:00 -0300

    test: ensure SurveyMongoRepository insert an questions on success

---
+++ src/infra/db/mongodb/survey/survey-mongo-respository.spec.ts
@@ -0,0 +1,44 @@
+import { Collection } from 'mongodb';
+import { MongoHelper } from '../helpers/mongo-helper';
+import { SurveyMongoRepository } from './survey-mongo-repository';
+
+let surveyCollections: Collection;
+
+describe('Survey Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    surveyCollections = await MongoHelper.getCollection('surveys');
+    await surveyCollections.deleteMany({}); // delete datas
+  });
+
+  const makeSut = (): SurveyMongoRepository => {
+    return new SurveyMongoRepository();
+  };
+
+  test('Should create survey', async () => {
+    const sut = makeSut();
+    await sut.add({
+      question: 'any_question',
+      answers: [
+        {
+          image: 'any_image',
+          answer: 'any_answer',
+        },
+        {
+          answer: 'other_answer',
+        },
+      ],
+    });
+    const survey = await surveyCollections.findOne({
+      question: 'any_question',
+    });
+    expect(survey).toBeTruthy();
+  });
+});
