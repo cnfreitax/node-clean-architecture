3c9df963b7fa9b2f11c2fca8e5fda3556d2b81e2
Victor Freitas <victornobrega1980@gmail.com>
2020-10-08 00:07:40 -0300

    feat: ensure SaveSurveyResultControllereturns 403 if invalid survey is provided

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller-protocols.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller-protocols.ts
@@ -2,3 +2,5 @@ export * from '../../../helpers/http/http-helpers';
 export * from '../../../protocols';
 export * from '../../../../domain/usecases/survey/load-survey-by-id';
 export * from '../../../../domain/models/survey';
+export * from '../../../helpers/http/http-helpers';
+export * from '../../../error';
--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -3,12 +3,19 @@ import {
   HttpRequest,
   HttpResponse,
   LoadSurveysById,
+  forbidden,
+  InvalidParamError,
 } from './save-survey-result-controller-protocols';
 
 export class SaveSurveyResultController implements Controller {
   constructor(private readonly loadSurveyById: LoadSurveysById) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadSurveyById.loadById(httpRequest.params.surveyId);
+    const survey = await this.loadSurveyById.loadById(
+      httpRequest.params.surveyId,
+    );
+    if (!survey) {
+      return forbidden(new InvalidParamError('surveyId'));
+    }
     return null;
   }
 }
