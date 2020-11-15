1e71d3e71423d4805c705466a3d52f57805ca4ba
Victor Freitas <victornobrega1980@gmail.com>
2020-11-15 03:06:30 -0300

    test: ensure LoadSurveyResult Controller returns 500 if throws

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.spec.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.spec.ts
@@ -3,9 +3,11 @@ import {
   forbidden,
   LoadSurveyById,
   InvalidParamError,
+  serverError,
 } from './load-survey-result-controller-protocols';
 import { LoadSurveyResultController } from './load-survey-result-controller';
 import { mockLoadSurveyById } from '../../../test';
+import { throwError } from '../../../../domain/test';
 
 type SutTypes = {
   sut: LoadSurveyResultController;
@@ -42,5 +44,14 @@ describe('LoadSurveyResult Controller', () => {
       .mockReturnValueOnce(Promise.resolve(null));
     const httpResponse = await sut.handle(mockFakeRequest());
     expect(httpResponse).toEqual(forbidden(new InvalidParamError('surveyId')));
+  });
+
+  test('Should return 500 is throws ', async () => {
+    const { sut, loadSurveyByIdStub } = makeSut();
+    jest
+      .spyOn(loadSurveyByIdStub, 'loadById')
+      .mockImplementationOnce(throwError);
+    const httpResponse = await sut.handle(mockFakeRequest());
+    expect(httpResponse).toEqual(serverError(new Error()));
   });
 });
