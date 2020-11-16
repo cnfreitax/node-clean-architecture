980c63720f8c09dde3b5ec975802e76863b92a91
Victor Freitas <victornobrega1980@gmail.com>
2020-11-15 21:49:16 -0300

    feat: add route to surve static files

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -2,9 +2,11 @@ import express from 'express';
 import setupSwagger from './config-swagger';
 import setupMiddlewares from './middlewares';
 import setupRoutes from './routes';
+import setupStaticFiles from './static-files';
 
 const app = express();
 setupSwagger(app);
+setupStaticFiles(app);
 setupMiddlewares(app);
 setupRoutes(app);
 
